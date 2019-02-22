bc3a3bde31c8449b8e3478fb3a28b0190fdf5a63
toxonaut <martin.schaerer@gmail.com>
2019-02-22 10:07:18 +0100

    Entered buying channel and changed adaptive sizing of text. Also changed channel funding to 25KSat

--- LightningMessage/Controllers/LightningController.cs
+++ LightningMessage/Controllers/LightningController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using System.IO;
 using System.Drawing.Imaging;
 using System.Text;
+using System.Linq;
 
 namespace LightningMessage.Controllers
 {
@@ -29,7 +30,21 @@ namespace LightningMessage.Controllers
 
         public IActionResult Index()
         {
+
             ViewData["Message"] = _myData.GetAppData.Message;
+            string[] words = _myData.GetAppData.Message.Split(" ");
+            int longestLength = words.OrderByDescending(s => s.Length).First().Length;
+            if (longestLength > 16)
+            {
+                double newsize = 150d / longestLength;
+                newsize = Math.Round(newsize, 2);
+                ViewData["sizer"] = "font-size: " + newsize + "vw";
+            }
+            else
+            {
+                ViewData["sizer"] = "font-size: 10vw";
+            }
+
             return View();
         }
 
@@ -98,6 +113,38 @@ namespace LightningMessage.Controllers
             return new JsonResult(new { billFilename = filename, billText = invoiceString });
         }
 
+        [Route("sendChannel/{*channelMemo}")]
+        public IActionResult SendChannel(string channelMemo)
+        {
+            Lightning lnd = GetLightning();
+            Google.Protobuf.ByteString RHash;
+            string invoiceString;
+            Bitmap qrCode;
+            int satoshis = 25000;
+
+            HttpContext.Session.Set("BuyChannel", Encoding.ASCII.GetBytes(channelMemo));
+
+            (invoiceString, qrCode, RHash) = lnd.GetInvoice((long)Convert.ToDouble(satoshis), "Buy inbound channel to " + channelMemo);
+            string filename = invoiceString.Substring(0, 20) + ".jpg";
+
+            string outputFileName = Path.Combine(_myData.BillsPath, filename);
+            HttpContext.Session.Set("QRFilenameChannel", Encoding.ASCII.GetBytes(outputFileName));
+
+            using (MemoryStream memory = new MemoryStream())
+            {
+                using (FileStream fs = new FileStream(outputFileName, FileMode.Create, FileAccess.ReadWrite))
+                {
+                    qrCode.Save(memory, ImageFormat.Jpeg);
+                    byte[] bytes = memory.ToArray();
+                    fs.Write(bytes, 0, bytes.Length);
+                }
+            }
+
+            byte[] hasharray = RHash.ToByteArray();
+            HttpContext.Session.Set("HashChannel", hasharray);
+            return new JsonResult(new { channelBillFilename = filename, channelBillText = invoiceString });
+        }
+
         private Lightning GetLightning()
         {
             Lightning lnd = new Lightning(_myData.CertLocation, _myData.MacLocation, _myData.lndGRPC);
@@ -152,6 +199,25 @@ namespace LightningMessage.Controllers
 
             }
             return new JsonResult(returnMessage);
+        }
+
+        [Route("checkpaidChannel")]
+        public async Task<IActionResult> CheckPaidChannel()
+        {
+            byte[] hasharray = HttpContext.Session.Get("HashChannel");
+            Google.Protobuf.ByteString RHash = Google.Protobuf.ByteString.CopyFrom(hasharray);
+            Lightning lnd = GetLightning();
+            await lnd.ResponseStreaming(RHash);
+            try
+            {
+                Email.WriteEmail("Me@lightningmessage.info", "martin.schaerer@gmail.com", "Open a channel", Encoding.ASCII.GetString(HttpContext.Session.Get("BuyChannel")));
+                System.IO.File.Delete(Encoding.ASCII.GetString(HttpContext.Session.Get("QRFilenameChannel")));
+            }
+            catch
+            {
+
+            }
+            return new JsonResult(new { paid = "paid.jpg", message = "Channel opening requested" });
         }
 
         [Route("checkmessageupdate/{message}")]
